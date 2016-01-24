a1f58a4336fbd0b8d319e66db032b9ed5c5472ab
cakeordeath83 <clair_griffiths@hotmail.com>
2016-01-24 09:22:25 +0000

    Wrote a couple of model tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "product attributes must not be empty" do
+		# Create a product with no attributes
+		product = Product.new
+		# Check that there is at least one error message for each validation that fails
+		assert product.invalid?
+		assert product.errors[:title].any?
+		assert product.errors[:description].any?
+		assert product.errors[:image_url].any?
+		assert product.errors[:price].any?
+	end
+	
+	test "product price must be positive" do
+		product = Product.new(title: "My Title", description: "My description", image_url: "test.jpg")
+		
+		# Test with a negative price
+		product.price = -1
+		assert product.invalid?
+		assert_equal ["must be greater than or equal to 0.01"], 
+			product.errors[:price]
+		
+		# Test with a price of 0		
+		product.price = 0
+		assert product.invalid?
+		assert_equal ["must be greater than or equal to 0.01"], 
+			product.errors[:price]
+		
+		# Test with a price of 1
+		product.price = 1
+		assert product.valid?
+	end
+	
 end

d80e4570311094e5228bb192d4c3af9a093cecd5
cakeordeath83 <clair_griffiths@hotmail.com>
2016-01-25 09:27:03 +0000

    Added styling and functional tests for Store controller

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,11 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+		# Also check that the page contains layout, product info and number formatting
+		assert_select "#columns #side a", minimum: 4
+		assert_select "#main .entry", 3
+		assert_select "h3", "MyString"
+		assert_select ".price", /\$[,\d]+\.\d\d/
   end
 
 end
